3613565dde70f1a80507b9ee452e3f563350d608
chela-droid <[200~chelangatcaroline70@gmail.com~>
2022-12-04 21:23:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code here!
+// const ul = document.getElementsByTagName("ul")[0];
+// const secondChild = ul.querySelector("li:nth-child(2)");
+// ul.removeChild(secondChild); 
+// main.remove(#main);
+// const element = document.getElementById('#main');
+// element.remove('#main');
+// remove('#main');
+
+main.remove();
+// let NewHeader = document.createElement();
+// newHeader.id = 'victory'
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+
+
+
+
+
+
+
+
+
+
+// var newHeader = document.getElementById('victory').innerHTML;
+// NewHeader.innerHTML = 'YOUR-NAME is the champion';
+// newHeader.setAttribute = 'h1#victory'
+
+// document.getElementById('victory').innerHTML
+// let newHeader = document.createElement("h1");
+
+// let newHeader = document.createElement("h1");
+//  newHeader.setAttribute("main","victory");
+// newHeader.victory = 'YOUR-NAME is the champion'
